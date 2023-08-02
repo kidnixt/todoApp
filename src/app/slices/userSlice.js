30fe22937c8f933e2790121938aa010ac4cc8f4a
kidnixt <kidnixt@gmail.com>
2023-08-02 20:00:39 -0300

    user slice

---
+++ src/app/slices/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user:null
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {},
+    reducers: {},
+})
